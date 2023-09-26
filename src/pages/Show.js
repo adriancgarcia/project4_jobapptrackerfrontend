90e2687dc5ebb655ee5c70f7d961009194a46af6
adriancgarcia <adriancgarcia@gmail.com>
2023-09-25 23:42:04 -0500

    fixing bugs

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -30,7 +30,7 @@ function Show (props) {
                     <input type="text" name="position" placeholder="Job Position" defaultValue={post.position}/>
                     <input type="text" name="company_name" placeholder="Company Name" defaultValue={post.company_name}/>
                     <input type="text" name="job_description" placeholder="Job Description" defaultValue={post.job_description}/>
-                    <input type="checkbox" name="applied" placeholder="Applied" defaultValue={post.applied}/>
+                    <h3>Did you apply?</h3><input type="checkbox" name="applied" placeholder="Applied" defaultValue={post.applied}/>
                     <input type="text" name="type_of_resume_sent" placeholder="ATS or Design Resume" defaultValue={post.type_of_resume_sent}/>
                     <input type="date" name="date_applied" placeholder="Date Applied" defaultValue={post.date_applied}/>
                     <input type="text" name="hiring_manager" placeholder="Hiring Manager" defaultValue={post.hiring_manager}/>
@@ -38,7 +38,7 @@ function Show (props) {
                     <input type="number" name="hiring_manager_phone" placeholder="Hiring Manager Phone #" defaultValue={post.hiring_manager_phone}/>
                     <input type="text" name="interview_status" placeholder="Interview Status" defaultValue={post.interview_status}/>
                     <input type="text" name="application_origin" placeholder="Where did you find application" defaultValue={post.application_origin}/>
-                    <input type="checkbox" name="thankyou_sent" placeholder="Thank you note sent" defaultValue={post.thankyou_sent}/>
+                    <h3>Did you send a thank you note?</h3><input type="checkbox" name="thankyou_sent" placeholder="Thank you note sent" defaultValue={post.thankyou_sent}/>
                     <button>Update Job</button>
                 </Form>
             </div>
